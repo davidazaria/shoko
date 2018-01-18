5e5b255a9663e95da556219a7329ff77ca962516
= <azaria.david@gmail.com>
2018-01-18 10:38:53 -0500

    pushing what I hope are the necessary webpack configs

---
+++ server.js
@@ -0,0 +1,35 @@
+!('NODE_ENV' in process.env) && require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', console.log('Hello World'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    error:
+    err,
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to: Andre ${PORT}`);
+});
