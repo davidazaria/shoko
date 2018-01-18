3036435f3111d4742bfaf6f2766994d7ae538c41
= <azaria.david@gmail.com>
2018-01-18 12:34:06 -0500

    testing

--- routes/route.js
+++ routes/route.js
@@ -3,5 +3,5 @@ const express = require('express');
 const shokoRoutes = express.Router();
 
 shokoRoutes.route('/');
-console.log('Hello route');
+  console.log('Hello route');
 module.exports = shokoRoutes;
