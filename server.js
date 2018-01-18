3139d753e324c3d8a578562bc54df4b3f97c2337
= <azaria.david@gmail.com>
2018-01-18 15:07:45 -0500

    setting up initial css and for eventual axios call

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/shoko', shokoRoutes);
-// app.use('/', (req, res) => res.send('Hello World, David'));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
 import Shoko from './components/Shoko';
 
 class App extends Component {
