c31d63b331ea1fecdb69fc89133bc7d536733855
= <azaria.david@gmail.com>
2018-01-18 11:38:34 -0500

    testing render

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+export default App;
