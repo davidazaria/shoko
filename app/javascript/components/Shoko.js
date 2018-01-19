deea51913e625509e8603a4c2f18ea1d53e046d9
= <azaria.david@gmail.com>
2018-01-19 09:56:31 -0500

    pushing something up to see if it works

---
+++ app/javascript/components/Shoko.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shoko = () => {
+return (
+  <div className="Shoko">
+  <h1> Shoko </h1>
+  <p> This is the beginning of the end </p>
+  </div>
+  );
+}
+export default Shoko;
