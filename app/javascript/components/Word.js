3f019b7f1383b5ee92d3cbc4532f52c36e6d2707
= <azaria.david@gmail.com>
2018-01-23 15:56:34 -0500

    had to dump etymology for now until i can handle the 404

--- app/javascript/components/Word.js
+++ app/javascript/components/Word.js
@@ -4,7 +4,7 @@ const Word = props => {
   console.log({ props });
   return (
     <div className="word">
-      <h1 id="word"> you entered {props.word.results[0].id}, which is pronounced {props.word.results[0].lexicalEntries[0].pronunciations[0].phoneticSpelling}.
+      <h1 id="ipa"> you entered the word {props.word.results[0].id}, which is pronounced {props.word.results[0].lexicalEntries[0].pronunciations[0].phoneticSpelling}.
       </h1>
     </div>
   );
