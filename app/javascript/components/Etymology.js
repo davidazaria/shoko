95dd770fef1f8529ffbe9cfabfa3757c27f1babf
= <azaria.david@gmail.com>
2018-01-26 15:46:02 -0500

    conditional render for etymologies work

--- app/javascript/components/Etymology.js
+++ app/javascript/components/Etymology.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Etymology = props => {
   console.log(props.etymology.word.results[0].lexicalEntries[0].entries[0]);
-  return !props.etymology.word.results[0].lexicalEntries[0].entries[0] !== 'etymologies' ?
+  return Object.keys(props.etymology.word.results[0].lexicalEntries[0].entries[0])[0] !== 'etymologies' ?
   <h1> no known etymology for this word at this time </h1> : (
     <div className="word">
       <h1 id="etymology"> the etymology of this word is {props.etymology.word.results[0].lexicalEntries[0].entries[0].etymologies[0]} </h1>
