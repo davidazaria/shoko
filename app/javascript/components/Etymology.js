45cc565371f4573706b479fee8648510be394354
= <azaria.david@gmail.com>
2018-01-26 15:58:52 -0500

    etymology is working

--- app/javascript/components/Etymology.js
+++ app/javascript/components/Etymology.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Etymology = props => {
-  console.log(props.etymology.word.results[0].lexicalEntries[0].entries[0]);
   return Object.keys(props.etymology.word.results[0].lexicalEntries[0].entries[0])[0] !== 'etymologies' ?
   <h1> no known etymology for this word at this time </h1> : (
     <div className="word">
